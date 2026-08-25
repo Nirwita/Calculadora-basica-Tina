1579d627338bc2e9f841d6d1532e9ca4211f863c
Nirwita <acasdigitalwork@gmail.com>
2026-08-25 20:02:00 +0100

    Agregados las funciones matematicas de multiplicar y dividir

--- js/calculadora.js
+++ js/calculadora.js
@@ -13,3 +13,51 @@ function sumar() {
   // Mostramos el resultado
   document.getElementById("resultado").innerHTML = resultado;
 }
+
+function restar() {
+  // Cogemos los valores de los dos inputs
+  let numero1 = document.getElementById("numero1").value;
+  let numero2 = document.getElementById("numero2").value;
+
+  // Convertimos los valores a números
+  numero1 = Number(numero1);
+  numero2 = Number(numero2);
+
+  // Hacemos la suma
+  let resultado = numero1 - numero2;
+
+  // Mostramos el resultado
+  document.getElementById("resultado").innerHTML = resultado;
+}
+
+function multiplicar() {
+  // Cogemos los valores de los dos inputs
+  let numero1 = document.getElementById("numero1").value;
+  let numero2 = document.getElementById("numero2").value;
+
+  // Convertimos los valores a números
+  numero1 = Number(numero1);
+  numero2 = Number(numero2);
+
+  // Hacemos la suma
+  let resultado = numero1 * numero2;
+
+  // Mostramos el resultado
+  document.getElementById("resultado").innerHTML = resultado;
+}
+
+function dividir() {
+  // Cogemos los valores de los dos inputs
+  let numero1 = document.getElementById("numero1").value;
+  let numero2 = document.getElementById("numero2").value;
+
+  // Convertimos los valores a números
+  numero1 = Number(numero1);
+  numero2 = Number(numero2);
+
+  // Hacemos la suma
+  let resultado = numero1 / numero2;
+
+  // Mostramos el resultado
+  document.getElementById("resultado").innerHTML = resultado;
+}
